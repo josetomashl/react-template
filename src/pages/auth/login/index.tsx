a7c309dcc9121b871b3629057ae1860484ffb61b
Jose Tomas Hernandez Lopez <57667471+JosetoHRN@users.noreply.github.com>
2025-05-23 20:18:39 +0200

    restore login page

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -1,21 +1,9 @@
-import { useDispatch } from 'react-redux';
 import styles from './styles.module.scss';
-import { NotificationType, pushNotification } from '@/store/modules/root';
 
 export function LoginPage() {
-  const dispatch = useDispatch();
-
-  const handleClick = (type: NotificationType, message: string) => {
-    dispatch(pushNotification({ message, type }));
-  };
-
   return (
     <div>
       <p className={styles.something}>Login page</p>
-      <button onClick={() => handleClick('info', 'awd')}>Click</button>
-      <button onClick={() => handleClick('warning', 'awd awdawd')}>Click</button>
-      <button onClick={() => handleClick('success', 'awd')}>Click</button>
-      <button onClick={() => handleClick('error', 'awd')}>Click</button>
     </div>
   );
 }
