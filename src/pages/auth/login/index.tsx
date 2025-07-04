cf7843caa249167ae0f1a50cb57964e5f76cb0e8
José Tomás Fiscoo <josetomas.hl@fiscoo.es>
2025-07-04 12:57:53 +0200

    add new hook and some use cases

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -1,6 +1,9 @@
+import { useTitle } from '@/hooks/useTitle';
 import styles from './styles.module.scss';
 
 export function LoginPage() {
+  useTitle('Login page');
+
   return (
     <div>
       <p className={styles.something}>Login page</p>
