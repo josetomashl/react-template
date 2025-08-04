62491ba136752886dd673eb0d053b2d8edde40fa
José Tomás Fiscoo <josetomas.hl@fiscoo.es>
2025-08-04 16:14:53 +0200

    fix translations context + hook

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,26 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-
 import '@/assets/styles/main.scss';
 import '@/assets/styles/reset.scss';
 import { NotificationsContainer } from '@/components/Notifications';
-import { AuthProvider } from '@/contexts/Auth';
+import { AuthProvider } from '@/contexts/authContext';
+import { LanguageProvider } from '@/contexts/translationContext';
 import { AppRoutes } from '@/routes';
 import { store } from '@/store';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <AuthProvider>
-          <AppRoutes />
-        </AuthProvider>
-        <NotificationsContainer />
-      </Provider>
-    </BrowserRouter>
+    <LanguageProvider>
+      <BrowserRouter>
+        <Provider store={store}>
+          <AuthProvider>
+            <AppRoutes />
+          </AuthProvider>
+          <NotificationsContainer />
+        </Provider>
+      </BrowserRouter>
+    </LanguageProvider>
   </StrictMode>
 );
