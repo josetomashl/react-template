d7c26cd0ec0e37141a645690808d844d496a85d6
José Tomás Fiscoo <josetomas.hl@fiscoo.es>
2025-08-05 17:16:58 +0200

    add dropdown basic component

---
+++ src/components/Dropdown/index.tsx
@@ -0,0 +1,101 @@
+import { css } from '@/utils';
+import { useState } from 'react';
+import { Icon } from '../Icon';
+import styles from './styles.module.scss';
+
+export interface DropdownItem {
+  label: string;
+  value: string;
+}
+
+interface Props {
+  label?: string;
+  items: DropdownItem[];
+  value: DropdownItem | null;
+  onChange: (value: DropdownItem | null) => void;
+  disabled?: boolean;
+  required?: boolean;
+  clearable?: boolean;
+  multiple?: boolean; // TODO
+  search?: boolean; // TODO
+}
+
+export function Dropdown({
+  label = 'Choose an option',
+  items,
+  value,
+  onChange,
+  disabled,
+  required,
+  clearable,
+  multiple,
+  search
+}: Props) {
+  const [isTouched, setIsTouched] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  const handleChange = (v: DropdownItem | null) => {
+    if (!disabled && onChange) {
+      if (!isTouched) {
+        setIsTouched(true);
+      }
+      onChange(v);
+      setIsOpen(false);
+    }
+  };
+
+  return (
+    <div className={styles.inputWrapper}>
+      <div
+        className={css(
+          styles.container,
+          isOpen ? styles.focused : '',
+          isTouched && !value && required ? styles.error : ''
+        )}
+        onClick={toggle}>
+        <div className={styles.inputContainer}>
+          <span className={styles.input}>{value ? value.label : label}</span>
+          <div className={styles.iconsContainer}>
+            {clearable && value && (
+              <div
+                className={styles.icon}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handleChange(null);
+                }}>
+                <Icon name='circleX' size={16} color='black' />
+              </div>
+            )}
+            <div className={styles.icon}>
+              <Icon name={isOpen ? 'chevronUp' : 'chevronDown'} size={16} color='black' />
+            </div>
+          </div>
+        </div>
+        {value && <span className={css(styles.label, value ? styles.floating : '')}>{label}</span>}
+        <div className={styles.dropdownContainer} onClick={(e) => e.stopPropagation()}>
+          {isOpen && (
+            <div className={styles.dropdownList}>
+              {items.map((item, index) => (
+                <div
+                  key={item.value}
+                  className={css(
+                    styles.dropdownItem,
+                    value?.value === item.value ? styles.selected : '',
+                    index > 0 ? styles.borderTop : ''
+                  )}
+                  onClick={() => handleChange(item)}>
+                  {item.label}
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+      <span className={styles.errorMessage}>{isTouched && !value && required ? 'This field is required' : ''}</span>
+    </div>
+  );
+}
