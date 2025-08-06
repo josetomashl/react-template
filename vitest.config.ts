2c10dae98a0706fd97155bf7d28dcaad6cbb0435
José Tomás Fiscoo <josetomas.hl@fiscoo.es>
2025-08-06 16:48:40 +0200

    fix vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,4 @@
+import path from 'node:path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
@@ -6,6 +7,9 @@ export default defineConfig({
     coverage: {
       enabled: true,
       reporter: ['text', 'json', 'html']
+    },
+    alias: {
+      '@': path.resolve(__dirname, './src')
     }
   }
 });
