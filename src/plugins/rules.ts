45cbee93bee6ed830b8499416d61abcd2299a84d
José Tomás Fiscoo <josetomas.hl@fiscoo.es>
2025-09-08 12:31:56 +0200

    add some more rules

--- src/plugins/rules.ts
+++ src/plugins/rules.ts
@@ -1,9 +1,14 @@
-const match = (v1: string, v2: string) => v1 === v2 || 'Las campos no coinciden';
-const max = (v: string, n = 3) => !v || v.length <= n || `Máximo ${n} caracteres`;
-const min = (v: string, n = 3) => !v || v.length >= n || `Mínimo ${n} caracteres`;
+const match = (v1: string, v2: string, fieldName?: string) =>
+  v1 === v2 || (fieldName ? `Debe coincidir con el campo "${fieldName}".` : 'Las campos no coinciden.');
+const maxLength = (v: string, n = 3) => !v || v.length <= n || `Máximo ${n} caracteres.`;
+const minLength = (v: string, n = 3) => !v || v.length >= n || `Mínimo ${n} caracteres.`;
+const max = (v: number, n: number) => !v || v <= n || `Debe ser menor o igual a ${n}`;
+const min = (v: number, n: number) => !v || v >= n || `Debe ser mayor o igual a ${n}`;
 
 export const RULES = {
   match,
+  maxLength,
+  minLength,
   max,
   min
 };
