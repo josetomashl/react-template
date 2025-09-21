2aef2263a7164e6edd380808df2c47de3cfb2917
Jose Tomas Hernandez Lopez <57667471+JosetoHRN@users.noreply.github.com>
2025-09-21 13:36:25 +0200

    fix linter

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -7,7 +7,7 @@ type HeaderItem = { key: string; label: string };
 
 type Props = {
   headers: HeaderItem[];
-  module: 'users' | 'posts';
+  module: 'posts'; // add more when necessary
   onPageChange: (page: number) => Promise<void> | void;
   onPageSizeChange: (pageSize: number) => Promise<void> | void;
 };
