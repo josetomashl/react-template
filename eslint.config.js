3626ad34b54560616b833a5273dcc2a1ec3420db
Jose Tomas Hernandez Lopez <57667471+JosetoHRN@users.noreply.github.com>
2025-10-09 20:58:29 +0200

    update config

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,20 @@
 import js from '@eslint/js';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  { ignores: ['dist', 'coverage'] },
+export default defineConfig([
+  globalIgnores(['dist', 'coverage']),
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended
+      // reactHooks.configs['recommended-latest'],
+      // reactRefresh.configs.vite
+    ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser
@@ -18,11 +24,10 @@ export default tseslint.config(
       'react-refresh': reactRefresh
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       '@typescript-eslint/no-unused-vars': 'warn',
       '@typescript-eslint/no-explicit-any': 'warn',
       'react-hooks/exhaustive-deps': 'off'
     }
   }
-);
+]);
