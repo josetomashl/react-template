329e84c88e8daa4aad1b4f810e186abc8a12cd5d
Jose Tomas Hernandez Lopez <57667471+JosetoHRN@users.noreply.github.com>
2026-07-14 21:35:49 +0200

    add ui Text component

---
+++ src/ui/Text/index.tsx
@@ -0,0 +1,72 @@
+import type { CSSProperties, ElementType, PropsWithChildren } from 'react';
+
+import { classes } from '@/utils';
+
+import styles from './index.module.scss';
+
+interface Props {
+  as?: ElementType;
+
+  size?: CSSProperties['fontSize'];
+  weight?: CSSProperties['fontWeight'];
+  lineHeight?: CSSProperties['lineHeight'];
+  color?: CSSProperties['color'];
+
+  align?: CSSProperties['textAlign'];
+  transform?: CSSProperties['textTransform'];
+
+  italic?: boolean;
+  underline?: boolean;
+  strike?: boolean;
+
+  wrap?: 'wrap' | 'nowrap' | 'balance';
+  truncate?: boolean;
+  noSelect?: boolean;
+
+  style?: CSSProperties;
+  className?: string;
+}
+
+export const Text = ({
+  children,
+  as: Component = 'p',
+  size,
+  weight,
+  lineHeight,
+  color = '#0a0a0a',
+  align = 'left',
+  transform = 'none',
+  wrap = 'wrap',
+  italic,
+  underline,
+  strike,
+  truncate,
+  noSelect,
+  style,
+  className
+}: PropsWithChildren<Props>) => {
+  return (
+    <Component
+      className={classes(
+        styles.text,
+        styles[`align-${align}`],
+        styles[`transform-${transform}`],
+        styles[`wrap-${wrap}`],
+        italic && styles.italic,
+        underline && styles.underline,
+        strike && styles.strike,
+        truncate && styles.truncate,
+        noSelect && styles.noSelect,
+        className
+      )}
+      style={{
+        fontSize: size,
+        fontWeight: weight,
+        lineHeight,
+        color,
+        ...style
+      }}>
+      {children}
+    </Component>
+  );
+};
