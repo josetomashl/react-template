87852f489e14429524434b813103b6f600dbcf3c
Jose Tomas Hernandez Lopez <57667471+JosetoHRN@users.noreply.github.com>
2026-07-21 20:32:04 +0200

    upgrade template and deps

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,11 @@
 /// <reference types="vite/client" />
 
+interface ViteTypeOptions {
+  // By adding this line, you can make the type of ImportMetaEnv strict
+  // to disallow unknown keys.
+  strictImportMetaEnv: unknown;
+}
+
 interface ImportMetaEnv {
   readonly VITE_SERVER_API: string;
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
-import react from '@vitejs/plugin-react';
+import babel from '@rolldown/plugin-babel';
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import { resolve } from 'node:path';
 import { defineConfig, loadEnv } from 'vite';
 
@@ -6,7 +7,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    plugins: [react()],
+    plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
     resolve: {
       alias: {
         '@': resolve(__dirname, './src')
